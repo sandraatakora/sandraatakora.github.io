3ea156940a5309d3691a49d6f5ee79f6985a5df6
Sandra Atakora <sandraatakora@Sandras-MacBook-Air.local>
2019-08-17 18:32:07 -0400

    p5 test

---
+++ sketch.js
@@ -0,0 +1,51 @@
+// P_1_0_01
+//
+// Generative Gestaltung – Creative Coding im Web
+// ISBN: 978-3-87439-902-9, First Edition, Hermann Schmidt, Mainz, 2018
+// Benedikt Groß, Hartmut Bohnacker, Julia Laub, Claudius Lazzeroni
+// with contributions by Joey Lee and Niels Poldervaart
+// Copyright 2018
+//
+// http://www.generative-gestaltung.de
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * changing colors and size by moving the mouse
+ *
+ * MOUSE
+ * position x          : size
+ * position y          : color
+ *
+ * KEYS
+ * s                   : save png
+ */
+'use strict';
+
+function setup() {
+  createCanvas(720, 720);
+  noCursor();
+
+  colorMode(HSB, 360, 100, 100);
+  //rectMode(CENTER); // Draws the rectangle from the center. What are other modes? Default is top-left.
+  noStroke();
+}
+
+function draw() {
+  background(mouseY / 2, mouseY/10, 100); // This is background color. 720/2 = 360 which is the color wheel. Only Hue changes originally. 
+
+  fill(360 - mouseY / 2, 100, 100);
+  rect(0, 0, 720, mouseY + 1); //This is the rect position/size – (x,y,w,h) – x,y is the starting point (top left unless rectMode is defined otherwise). w,h, is widht and height in px.
+  // Why + 1? Must be some sort of best practice?? Works fine without it in this example.
+}
+
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
